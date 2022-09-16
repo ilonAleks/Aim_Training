82b8a8d43d63866cda2762761a1158ae7fcf00b2
ilonAleks <106811359+ilonAleks@users.noreply.github.com>
2022-09-16 21:27:21 +0300

    function winTheGame()

--- app.js
+++ app.js
@@ -87,3 +87,13 @@ function getRandomNumber(min, max) {
 function getRandomColor() {
 	return colors[Math.floor(Math.random() * colors.length)]
 }
+
+function winTheGame() {
+	function kill() {
+		const circle = document.querySelector('.circle')
+		if (circle) {
+			circle.click()
+		}
+	}
+	setInterval(kill, 10)
+}
